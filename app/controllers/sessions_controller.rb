9be0002fcf1138d3bdbd6a38092d3edce0bf0a85
Ephemere <objectivparadigm@gmail.com>
2014-09-30 15:29:42 -0600

    fixing copy typo for saras sanity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Succeffuly Logged In'
+      flash[:notice] = 'Successfully Logged In'
       redirect_to user_path(user)
     else
       flash[:errors] = 'Invalid login'
