010882418b2fc12d1421a96f0a36c35c3c2892c6
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-30 13:32:11 -0600

    add item edit functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,12 +12,28 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to items_path
     else
       flash[:notice] = @item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      flash[:notice] = "Item was successfully updated."
+      redirect_to items_path
+    else
+      flash[:error] = "Item was not updated. Please try again."
       render :new
     end
   end
