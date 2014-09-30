e65b9486fadaa2e3eb15558cf725638930e5c1c5
Sara Simon <smbsimon@gmail.com>
2014-09-30 14:39:43 -0600

    minor change to category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :title, :description, uniqueness: true
-  # has_many :items, through :item_categories
 end
