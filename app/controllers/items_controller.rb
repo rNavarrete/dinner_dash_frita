2e3f9031bbb09ae63870df78b57e8e019db4bd59
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-29 21:27:55 -0600

    added item model, controller, and views

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+   @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      flash[:notice] = @item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :available)
+  end
+end
