881c1c44c1522f5dad7d36d093acf099a89262c8
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-29 22:31:26 -0600

    nothing changed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+  has_attached_file :image
   validates :title, :description, presence: true, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :image_file_name, allow_blank: true, format: {
