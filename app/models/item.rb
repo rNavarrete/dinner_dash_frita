dcec7c04c8a4984b2b7c3f4cb1ba8c6475c5689e
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-05 17:23:19 -0600

    fixed alignment issues with the menu items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   has_many :item_categories, dependent: :destroy
   has_many :categories, through: :item_categories
 
-  has_attached_file :image, :styles => { :large => "600x600", :medium => "240x275>", :thumb => "100x100>" }, :default_url => "http://fillmurray.com/175/175"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://fillmurray.com/252/158"
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates :title, :description, presence: true, uniqueness: true
