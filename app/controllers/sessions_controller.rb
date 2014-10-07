903169071d7b969a4475361ed9816058eaeb6788
Sara Simon <smbsimon@gmail.com>
2014-10-06 18:45:04 -0600

    add admin route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password]) && user.admin?
       session[:user_id] = user.id
       flash[:notice] = 'Successfully Logged In'
-      redirect_to user_orders_path(user)
+      # redirect_to user_orders_path(user)
+      redirect_to admin_path
     elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = 'Successfully Logged In'
