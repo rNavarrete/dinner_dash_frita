a2728c9a306df75ea0e9261bd20e358397cec647
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-10-06 19:16:59 -0600

    Update user model validations for email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :username, length: { in: 2..32, allow_nil: true }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 
   def admin?
     admin == true
