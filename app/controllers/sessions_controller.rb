bc155584109ff884b5c117f10df7a67ff584b15f
Sara Simon <smbsimon@gmail.com>
2014-10-06 18:38:06 -0600

    fix route for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password]) && user.admin?
       session[:user_id] = user.id
       flash[:notice] = 'Successfully Logged In'
       redirect_to user_orders_path(user)
+    elsif user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Successfully Logged In'
+      redirect_to root_path
     else
       flash[:errors] = 'Invalid login. Please try again.'
       redirect_to root_path
