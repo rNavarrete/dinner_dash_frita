a20deca51511b2a8b1bcdf7dae70ff64096d957a
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-10-07 13:44:40 -0600

    Add cart resources to master

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930204046) do
+ActiveRecord::Schema.define(version: 20141007193535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -41,7 +46,30 @@ ActiveRecord::Schema.define(version: 20140930204046) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "available",                                  default: true
+    t.string   "status",                                     default: "active"
   end
+
+  create_table "order_items", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "order_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.string   "pickup_or_delivery"
+    t.string   "street_number"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -49,6 +77,8 @@ ActiveRecord::Schema.define(version: 20140930204046) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",           default: false
+    t.string   "email"
+    t.string   "username"
   end
 
 end
