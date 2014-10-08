64f49a7ae9d065cc3e1483c2ab032e9b2a8aeb4c
Josh Cheek <josh.cheek@gmail.com>
2014-10-08 10:06:37 -0600

    Create a use case for fixing the cart/user relationships

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password]) && user.admin?
       session[:user_id] = user.id
       flash[:notice] = 'Successfully Logged In'
-      # redirect_to user_orders_path(user)
+      NormalizeUserCart.call(user, session)
       redirect_to admin_path
     elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -24,13 +24,5 @@ class SessionsController < ApplicationController
     session.clear
     flash[:notice] = 'Successfully Logged Out.'
     redirect_to root_path
-  end
-
-  private
-
-  def fix_cart_shit(user)
-    # make sure that if they already have a cart, it's associated to their user
-    # if they don't have a cart in their session or on their user, make them one
-    # if they have a cart_id in session, and a cart on their user (and they're not the same cart)... consolidate or ignore or w/e
   end
 end
---
+++ app/use_cases/normalize_user_cart.rb
@@ -0,0 +1,7 @@
+module NormalizeUserCart
+  def self.call(user, session)
+    # make sure that if they already have a cart, it's associated to their user
+    # if they don't have a cart in their session or on their user, make them one
+    # if they have a cart_id in session, and a cart on their user (and they're not the same cart)... consolidate or ignore or w/e
+  end
+end
