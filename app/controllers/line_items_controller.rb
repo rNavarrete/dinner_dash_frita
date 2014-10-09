8fbd2da96e472f623dd5523e5a28bfb40b144caf
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-10-09 09:54:06 -0600

    Update cart methods and views

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,17 +5,21 @@ class LineItemsController < ApplicationController
 
   def create
     cart.add_line_item(params[:id], params[:quantity][:quantity])
-    flash[:notice] = "#{cart.line_items}"
+    flash[:notice] = "#{params[:quantity][:quantity]} #{find_item(params[:id]).title}".pluralize + " sucessfully added to your cart!"
     redirect_to root_path
-    # item = Item.find(params[:id])
-    # cart.add_line_item(params[:id], params[:quantity][:quantity])
-    # line_item = LineItem.create(item_id: item.id, quantity: params[:quantity][:quantity])
-    # cart.items.merge!(line_item: line_item)
-
-    # session[:cart_items] << [item, params[:quantity][:quantity] ]
-    # flash[:notice] = "#{params[:quantity][:quantity]} " +  "#{item.title}".pluralize +  " successfully added to cart. #{session[:cart_items]}"
-    # redirect_to :back
    end
+
+  def update
+    update_item(params[:id], params[:quantity])
+    flash[:notice] = "{find_item(params[:id]).title} has been successfully changed"
+    redirect_to cart_path
+  end
+
+  def destroy
+    delete_item(params[:id])
+    flash[:notice] = "#{find_item(params[:id]).title} has been successfully deleted."
+    redirect_to cart_path
+  end
 
   private
 
