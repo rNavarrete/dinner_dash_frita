f8e6811d8df70df95af48d04d3a69fb98375f51d
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-10-09 17:47:12 -0600

    Remove available column from items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009172712) do
+ActiveRecord::Schema.define(version: 20141009233403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20141009172712) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.boolean  "available",                                  default: true
     t.string   "status",                                     default: "active"
   end
 
