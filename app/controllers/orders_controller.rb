f0bcf5ec67139d7ceb0011cca3f4c76e1ede545f
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-11 19:46:16 -0600

    fixed pickup or delivery option

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,10 @@ class OrdersController < ApplicationController
 
   def new
     @user = User.new
-    @order = Order.new
+    @order = Order.new(pickup_or_delivery: params[:pickup_option])
     @line_items = line_items
     @address = Address.find_by(user_id: current_user.id)
+    @pickup_option = params[:pickup_option]
   end
 
   def create
