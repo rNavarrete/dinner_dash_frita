abed3ea0740c1e841a1249e20e17bd5d3c0e4c2b
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-12 01:26:32 -0600

    rough working version of users orders and item view pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,9 @@ class ApplicationController < ActionController::Base
   helper_method :address_delivered_to
   helper_method :order_items
   helper_method :line_item_subtotal
+  helper_method :past_order_quantity
+  helper_method :past_order_total
+
 
   def cart
   	@cart ||= Cart.new(session)
@@ -48,11 +51,11 @@ class ApplicationController < ActionController::Base
    end
 
    def update_item(item_id, quantity)
-   	line_items[item_id] = quantity
+   		line_items[item_id] = quantity
    end
 
    def delete_item(item_id)
-   	line_items.delete(item_id)
+   		line_items.delete(item_id)
    end
 
    def valid_state_code
@@ -70,17 +73,29 @@ class ApplicationController < ActionController::Base
 
    # refactor this shit
    def order_items(line_items_hash)
-   	items = {}
-   	line_items_array = line_items_hash.to_a
-   	line_items_array.each do |id_and_item_array|
-   		item_id = id_and_item_array[0].scan(/\d+/)
-   		quantity = id_and_item_array[1].to_i
-   		items[item_id[0]] = quantity
-   	end
+   		items = {}
+   		line_items_array = line_items_hash.to_a
+   		line_items_array.each do |id_and_item_array|
+   			item_id = id_and_item_array[0].scan(/\d+/)
+   			quantity = id_and_item_array[1].to_i
+   			items[item_id[0]] = quantity
+   		end
    		items
    end
 
    def line_item_subtotal(item_id, quantity)
    		find_item(item_id).price *  quantity
    end
+
+   def past_order_quantity(line_items_hash)
+   		order_items(line_items_hash).values.reduce(0) {|sum, quantity| sum += quantity}
+   end
+
+   def past_order_total(line_items_hash)
+ 		total = 0
+   		order_items(line_items_hash).each do |item_id, quantity|
+    		total += find_item(item_id).price * quantity.to_i
+    	end
+    total
   end
+end
